578ecc856b0e1162ffd545aef078397cdadcca42
Bubber Slem <cph-mr671@cphbusiness.dk>
2025-09-28 23:26:41 +0200

    push

--- src/main/java/app/dtos/RoomDTO.java
+++ src/main/java/app/dtos/RoomDTO.java
@@ -2,34 +2,44 @@ package app.dtos;
 
 import app.entities.Room;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.List;
+
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RoomDTO {
-    private int id;
-    private int number;
-    private double price;
-    private int hotelId; // så vi ved hvilket hotel rummet hører til
 
-    // Entity -> DTO
+    private Integer id;
+    private Integer hotelId;
+    private Integer number;
+    private double price;
+    private String type;
+
+
     public RoomDTO(Room room) {
         this.id = room.getId();
+        this.hotelId = room.getHotel().getId();
         this.number = room.getNumber();
         this.price = room.getPrice();
-        this.hotelId = room.getHotel() != null ? room.getHotel().getId() : 0;
+        this.type = room.getType();
     }
 
-    // DTO -> Entity (valgfri)
-    public Room toEntity() {
-        Room room = new Room();
-        room.setId(this.id);
-        room.setNumber(this.number);
-        room.setPrice(this.price);
-        return room;
+    public RoomDTO(Integer id, Integer hotelId, double price, Integer number, String type) {
+        this.id = id;
+        this.hotelId = hotelId;
+        this.price = price;
+        this.number = number;
+        this.type = type;
+    }
+
+    public static List<RoomDTO> toDTOList(List<Room> resultList) {
+        return resultList.stream().map(RoomDTO::new).toList();
     }
 }
